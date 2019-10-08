67530c6a992aaa23fc20daedc3f973fd0e780dc9
mhezzet <40916821+mhezzet@users.noreply.github.com>
2019-10-08 21:25:33 +0200

    requested list of items

--- cafe/src/pages/Landing.jsx
+++ cafe/src/pages/Landing.jsx
@@ -1,5 +1,30 @@
 import React from 'react'
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/react-hooks'
 
 export default function Landing() {
+  const { data, error, loading } = useQuery(ITEMS)
+
+  if (loading) return <div>loading</div>
+  if (error) return <div>{error.message}</div>
+
+  console.log(data)
+
   return <div>hi iam landing page</div>
 }
+
+const ITEMS = gql`
+  query {
+    items {
+      id
+      name
+      price
+      type
+      photo {
+        id
+        url
+        key
+      }
+    }
+  }
+`
